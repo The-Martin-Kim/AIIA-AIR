6597ba5de7148296cc2843f5a7355fcbf8d6c012
Martin Kim <127666042+The-Martin-Kim@users.noreply.github.com>
2023-06-06 15:34:22 +0900

    FIX: UserName NULL fixed

--- lib/chatting/message.dart
+++ lib/chatting/message.dart
@@ -30,7 +30,7 @@ class Messages extends StatelessWidget {
             return ChatBubbles(
                 chatDocs[index].data()['text'],
                 chatDocs[index].data()['userID'].toString() == user!.uid,
-                chatDocs[index].data()['userName'].toString());
+                chatDocs[index].data()['userData'].toString());
           },
         );
       },
